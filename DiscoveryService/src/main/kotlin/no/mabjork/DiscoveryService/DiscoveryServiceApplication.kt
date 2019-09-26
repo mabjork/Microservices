18694cb6830a78a15c1580e2d89074c765ba9fe6
Marius Bjørkli <marius@Mariuss-MacBook-Pro.local>
2019-09-26 20:06:11 +0200

    Initial commit

---
+++ DiscoveryService/src/main/kotlin/no/mabjork/DiscoveryService/DiscoveryServiceApplication.kt
@@ -0,0 +1,12 @@
+package no.mabjork.DiscoveryService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class DiscoveryServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiscoveryServiceApplication>(*args)
+}
