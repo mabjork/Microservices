c692af1b93aa8eec1f52a2d0460f7321f83ddd1b
Marius Bjørkli <marius@Mariuss-MacBook-Pro.local>
2019-10-14 20:28:48 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,33 @@
+
+
+for i in "$@"
+do
+  if [ "$i" = "-c" ]
+  then
+    buildContainers=true
+  fi
+
+  if [ "$i" = "-DskipTest" ]
+  then
+    skipTests=true
+  fi
+done
+
+while read service; do
+  arguments="-q"
+
+  if [[ $skipTests = true ]]
+  then
+    arguments=$arguments" -DskipTest"
+  fi
+
+  cd ./$service
+  mvn package $arguments
+  cd ..
+
+done < services
+
+if [[ $buildContainers = true ]]
+then
+  docker-compose up
+fi
